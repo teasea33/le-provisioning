0a767c1d8efecbeaabaaaa3c94b15c303c835dda
Nick Bagley <nick@thrivehive.com>
2016-04-15 08:09:51 -0700

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = 'le-provisoning',
+    packages = ['le-provisioning'],
+    version = '0.1',
+    description = 'A small set of scripts to install SSL certs on WHM using LetsEncrypt',
+    author = 'Nick Bagley',
+    author_email = 'nick@thrivehive.com',
+    url = ''
+)
