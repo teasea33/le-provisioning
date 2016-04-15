05a7fc14ca5a8ac56f4a2d14cda1f6a4cf68a893
Nick Bagley <nick@thrivehive.com>
2016-04-15 10:29:07 -0700

    Changes from pypi instructions

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
 from distutils.core import setup
+
 setup(
-    name = 'le-provisoning',
-    packages = ['le-provisioning'],
-    version = '0.1',
-    description = 'A small set of scripts to install SSL certs on WHM using LetsEncrypt',
-    author = 'Nick Bagley',
-    author_email = 'nick@thrivehive.com',
-    url = ''
+    name='le-provisioning',
+    packages=['le-provisioning'],
+    version='0.1',
+    description='A small set of scripts to install SSL certs on WHM using LetsEncrypt',
+    author='Nick Bagley',
+    author_email='nick@thrivehive.com',
+    url='https://github.com/teasea33/le-provisioning.git',
+    download_url='https://github.com/teasea33/le-provisioning/archive/0.1.zip',
+    keywords=['letsencrypt', 'ssl', 'whm', 'cpanel'],
+    license='GPLv3',
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux",
+        "Development Status :: 2 - Pre-Alpha"
+    ]
 )
